71d4843066655bf32accfc626e286170e46cfee5
da-konopka <da.konopka@student.po.edu.pl>
2019-12-29 02:48:34 +0100

    Add ProductInfoActivity

---
+++ app/src/main/java/com/example/javaallegroapi/ProductInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.javaallegroapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ProductInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_info);
+    }
+}
